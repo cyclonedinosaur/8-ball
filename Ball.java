0d5de71cc3b98252741c2a14a5da868fb651fdb7
cyclonedinosaur <45861067+cyclonedinosaur@users.noreply.github.com>
2019-03-11 13:04:14 -0700

    Add files via upload

--- Ball.java
+++ Ball.java
@@ -1,4 +1,27 @@
+import javax.swing.JLabel;
 
 public class Ball {
 
+	protected int x;//x pos
+	protected int y;//y pos
+	protected int r;//radius
+	protected int s;//size
+	protected int vx;//change in x
+	protected int vy;//change in y
+	protected JLabel img;
+	public Ball() {
+		
+	}
+	
+	public void move() {
+		x += Math.sqrt(vx^2 + vy^2);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
 }
